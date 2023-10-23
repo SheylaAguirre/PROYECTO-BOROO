6901a78e05f8c7b0bf61edf787b3747a844c54c6
pepperoniego1247 <80595584+pepperoniego1247@users.noreply.github.com>
2023-10-22 21:47:09 -0500

    Add files via upload

---
+++ capaLogicaOC/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaLogicaOC
+{
+    public class Class1
+    {
+        
+    }
+}
---
+++ capaLogicaP/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaLogicaP
+{
+    public class Class1
+    {
+    }
+}

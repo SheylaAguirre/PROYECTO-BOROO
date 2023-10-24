f852a1d83f0573acc7c6afb5022088b1b0b0be2d
pepperoniego1247 <80595584+pepperoniego1247@users.noreply.github.com>
2023-10-23 23:04:40 -0500

    Add files via upload

--- capaPresentacion/formOrdenCompra.cs
+++ capaPresentacion/formOrdenCompra.cs
@@ -28,5 +28,11 @@ namespace capaPresentacion
             formMantenedorCotizacion formCotizacion = new formMantenedorCotizacion();
             formCotizacion.Show();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            formPresupuesto formPresupuesto = new formPresupuesto();
+            formPresupuesto.Show(); 
+        }
     }
 }
